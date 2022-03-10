aaf4f3795f2ebecb0f5344b69ee8cd083512051d
Sang Shin <sangshinpassion@gmail.com>
2022-03-10 12:59:32 -0500

    namedparameter code is added

--- hibernate-exercise/src/main/java/com/test/hib/model/Employee.java
+++ hibernate-exercise/src/main/java/com/test/hib/model/Employee.java
@@ -6,8 +6,29 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
+
 @Entity
 @Table
+//Using @NamedQuery for single  HQL
+@NamedQuery(name = "getallEmployee", query = "select e from Employee e")
+
+//Using @NamedQueries for multiple  HQL
+@NamedQueries({
+        @NamedQuery(name = "get_Emp_name_by_id", query = "select e.name,e.salary,e.job from Employee e where id=:id"),
+        @NamedQuery(name = "get_all_dept", query = "from Employee"),
+        @NamedQuery(name = "findEmployeeByName", query = "from Employee e where e.name = :name"),
+        @NamedQuery(name = "empDepAlias", query = "select e, e.officeCode , e.name, CASE When (e.officeCode = '1') THEN 'IT'"
+                + "WHEN (e.officeCode = 6) THEN 'Admin'"
+                + "WHEN (e.officeCode = 5) THEN 'HR'"
+                + "WHEN (e.officeCode = 4) THEN 'Developers'"
+                + "WHEN(e.officeCode = 3) THEN 'Accounts'"
+                + "WHEN (e.officeCode = 2) THEN 'Finanace'"
+                + "ELSE 'General' END FROM Employee e  "),
+})
+
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,59 +40,89 @@ public class Employee {
     private String zipcode;
     private String city;
     private Date startDate;
+    private int officeCode;
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getSalary() {
         return salary;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
     public String getJob() {
         return job;
     }
+
     public void setJob(String job) {
         this.job = job;
     }
+
     public String getAddressLine() {
         return addressLine;
     }
+
     public void setAddressLine(String addressLine) {
         this.addressLine = addressLine;
     }
+
     public String getZipcode() {
         return zipcode;
     }
+
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public Date getStartDate() {
         return startDate;
     }
+
     public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
+
+    public int getOfficeCode() {
+        return officeCode;
+    }
+
+    public void setOfficeCode(int officeCode) {
+        this.officeCode = officeCode;
+    }
+
     @Override
     public String toString() {
-        return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", job=" + job + ", addressLine="
-                + addressLine + ", zipcode=" + zipcode + ", city=" + city + ", startDate=" + startDate + "]";
+        return "Employee [id=" + id + ", " +
+                "name=" + name + ", " +
+                "salary=" + salary + ", " +
+                "job=" + job + ", " +
+                "addressLine=" + addressLine + ", " +
+                "zipcode=" + zipcode + ", " +
+                "city = " + city + ", " +
+                "startDate = " + startDate + "]";
     }
 }
-
-
--- hibernate-exercise/src/main/java/com/test/hib/namedQueries/EmployeeController.java
+++ hibernate-exercise/src/main/java/com/test/hib/namedQueries/EmployeeController.java
@@ -5,6 +5,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
+
+import javax.persistence.TypedQuery;
+import java.util.Iterator;
+import java.util.List;
 
 public class EmployeeController {
     public void createEmployeeTable() {
@@ -18,13 +23,86 @@ public class EmployeeController {
         factory.close();
         session.close();
     }
+
+    public void populateEmployeeTable() {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        Transaction t = session.beginTransaction();
+
+        String nativeSQL =
+                "INSERT INTO `mydb`.`employee` (`addressLine`, `city`, `job`, `name`, `officeCode`, `salary`, `startDate`, `zipcode`) VALUES \n" +
+                        "('24-10', 'NYC', 'Ceo', 'Tom Thele', 1, '25600', '2021-09-09 18:32:11.000000', '11102'),\n" +
+                        "('35-16', 'LA', 'Manager', 'Jenny Ji', 2, '15600', '2021-09-09 18:32:11.000000', '11103'),\n" +
+                        "('34-10', 'NJ', 'Cfo', 'M Joseph', 3, '16600', '2021-09-09 18:32:11.000000', '11583'),\n" +
+                        "('44-20', 'NYC', 'Manager', 'James Judge', 4, '185600', '2021-09-09 18:32:11.000000', '18983'),\n" +
+                        "('44-20', 'Dallas', 'Manager', 'Ramon Rio', 5, '36600', '2021-09-09 18:32:11.000000', '14783'),\n" +
+                        "('44-40', 'LA', 'Manager', 'James Santana', 6, '78600', '2021-09-09 18:32:11.000000', '18483'),\n" +
+                        "('484-40', 'LA', 'Manager', 'Elded Oreo', 6, '50089', '2021-09-09 18:32:11.000000', '155483')";
+        NativeQuery nativeQuery = session.createSQLQuery(nativeSQL);
+        nativeQuery.executeUpdate();
+
+        t.commit();
+        System.out.println("successfully populated");
+        factory.close();
+        session.close();
+    }
+
+    public void findEmployeeByname() {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+//------------  Hibernate Named Query   -------------
+        TypedQuery query = session.getNamedQuery("findEmployeeByName");
+        query.setParameter("name", "Tom Thele");
+        List<Employee> employees = query.getResultList();
+        Iterator<Employee> itr = employees.iterator();
+        while (itr.hasNext()) {
+            Employee e = itr.next();
+            System.out.println(e);
+        }
+        factory.close();
+        session.close();
+    }
+
+    public void findEmployeeById() {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+//------------  Hibernate Named Query   -------------
+        TypedQuery query = session.getNamedQuery("get_Emp_name_by_id");
+        query.setParameter("id", 3);
+        List<Object[]> emName = query.getResultList();
+
+        for (Object[] o : emName) {
+            System.out.println("Employee name: " + o[0]
+                    + " | Employee Salary: " + o[1]
+                    + " | Emp Job Title: " + o[2]);
+        }
+
+        factory.close();
+        session.close();
+    }
+
+    public void ShowOfficeCodes_AsDepartment() {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+//------------  Hibernate Named Query   -------------
+        TypedQuery query = session.getNamedQuery("empDepAlias");
+        List<Object[]> list = query.getResultList();
+        for (Object[] e : list) {
+            System.out.println("OfficeCode: " + e[1] + " | Dep Name: " + e[3] + " | Employee Name: " + e[2]);
+        }
+        factory.close();
+        session.close();
+    }
 }
+
 
 class App {
     public static void main(String[] args) {
-        System.out.println( "Hello World!" );
         EmployeeController e = new EmployeeController();
         e.createEmployeeTable();
+        e.populateEmployeeTable();
+        e.findEmployeeByname();
     }
 }
 
