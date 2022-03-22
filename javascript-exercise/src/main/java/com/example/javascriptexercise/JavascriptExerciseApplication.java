9d8d57b76342e16945ac1451b17f57cd36f8cda7
Sang Shin <sangshinpassion@gmail.com>
2022-03-22 07:09:00 -0400

    javascript exercise added

---
+++ javascript-exercise/src/main/java/com/example/javascriptexercise/JavascriptExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javascriptexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavascriptExerciseApplication.class, args);
+    }
+
+}
---
+++ javascript-exercise/src/test/java/com/example/javascriptexercise/JavascriptExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascriptexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavascriptExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
