60658a35e2dd130724e0aa4dbc65e5c09402e3de
Sang Shin <sangshinpassion@gmail.com>
2022-03-28 08:26:23 -0400

    validation form updated

---
+++ javascript-exercise/form-validation/script/script2.js
@@ -0,0 +1,62 @@
+let element = document.getElementById("form");
+let ele = document.getElementById("feedback");
+if (element.addEventListener) {
+    element.addEventListener("submit", main_validator, true);
+}
+else if (ele.attachEvent) {
+    element.attachEvent('onsubmit', main_validator)
+}
+
+function main_validator(e) {
+    console.log(e);
+    e.preventDefault();
+    ele.innerHTML = "";
+    if (check_username(e.target[0].value) &&
+        check_dob(e.target[1].value)
+    ) {
+        element.submit();
+    }
+}
+
+function check_username(username) {
+    let re = new RegExp(/^[a-zA-Z0-9_.-]*$[a-zA-Z_.-]*$/g);
+    let message = "";
+    let someWrong = false;
+    console.log(re.test(username));
+    if (!re.test(username)) {
+        message += "username can only be [a-z], [A-Z], or (-,_)";
+        someWrong = true;
+    }
+    if (username.length > 20) {
+        message += "username must be less than 20 characters";
+        someWrong = true;
+    }
+    if (someWrong) {
+        let para = document.createElement("P");
+        para.classList.add("danger");
+        let t = document.createTextNode(message);
+        para.appendChild(t);
+        ele.appendChild(para);
+        return false;
+    }
+    return true;
+}
+
+function check_dob(dob) {
+    let re = new RegExp(/^(0[1-9]|1[012])[-/.](0[1-9]|[12][0-9]|3[01])[-/.](19|20)\\d\\d$/);
+    let message = "";
+    let someWrong = false;
+    if (!re.test(dob)) {
+        message += "your date of birth is not in the right format";
+        someWrong = true;
+    }
+    if (someWrong) {
+        let para = document.createElement("P");
+        para.classList.add("danger");
+        let t = document.createTextNode(message);
+        para.appendChild(t);
+        ele.appendChild(para);
+        return false;
+    }
+    return true;
+}
