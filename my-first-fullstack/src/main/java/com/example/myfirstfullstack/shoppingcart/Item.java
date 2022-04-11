3d8b14cac0155bbdfe542414f4b510916a1471fd
Sang Shin <sangshinpassion@gmail.com>
2022-04-11 18:10:12 -0400

    session example code added

---
+++ my-first-fullstack/src/main/java/com/example/myfirstfullstack/shoppingcart/Item.java
@@ -0,0 +1,21 @@
+package com.example.myfirstfullstack.shoppingcart;
+
+public class Item {
+
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
