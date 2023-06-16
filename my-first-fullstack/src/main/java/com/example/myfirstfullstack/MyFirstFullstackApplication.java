78581a8479c67ab8ade75cc3471ef58f2463c919
Sang Shin <sangshinpassion@gmail.com>
2023-06-16 05:38:08 -0400

    use h2 database

--- my-first-fullstack/src/main/java/com/example/myfirstfullstack/MyFirstFullstackApplication.java
+++ my-first-fullstack/src/main/java/com/example/myfirstfullstack/MyFirstFullstackApplication.java
@@ -1,14 +1,10 @@
 package com.example.myfirstfullstack;
 
-import com.example.myfirstfullstack.controller.EmployeeController;
-import com.example.myfirstfullstack.services.DummyService;
 import com.example.myfirstfullstack.services.EmployeeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
