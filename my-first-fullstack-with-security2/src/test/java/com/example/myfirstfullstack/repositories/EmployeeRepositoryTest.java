eceb38cbd2275dcfcd829d648f171e4f209e3c8a
Sang Shin <sangshinpassion@gmail.com>
2023-06-23 04:50:02 -0400

    change

--- my-first-fullstack-with-security2/src/test/java/com/example/myfirstfullstack/repositories/EmployeeRepositoryTest.java
+++ my-first-fullstack-with-security2/src/test/java/com/example/myfirstfullstack/repositories/EmployeeRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.myfirstfullstack.repositories;
 
 import com.example.myfirstfullstack.models.Employee;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,15 @@ class EmployeeRepositoryTest {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    void setUp() {
+        Employee employee = new Employee();
+        employee.setLastName("Jones");
+        employee.setFirstName("Tom");
+        employee.setEmail("tom.jones@gmail.com");
+        employeeRepository.save(employee);
+    }
 
     @Test
     void findFirstByLastName_should_return_Employee_given_valid_lastname() {
